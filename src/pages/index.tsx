dfa58fc1073206015391a9108b5ff5a8d411edeb
Walisson Silva <walissonsilva10@gmail.com>
2021-03-23 09:14:20 -0300

    chore: add scss global styles and add README.md

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+
+      <h1>Hello World!</h1>
+    </>
   )
 }
