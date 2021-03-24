4e1c41404b8cbdcc5341fdf961a4f4317e1fa179
Walisson Silva <walissonsilva10@gmail.com>
2021-03-24 08:31:15 -0300

    feat: home page front-end done

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import SignInButton from '../SignInButton';
+import styles from './styles.module.scss';
+
+export function Header() {
+  return(
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="Logo do ig.news"/>
+
+        <nav>
+          <a className={styles.active} href="/">Home</a>
+          <a href="/">Posts</a>
+        </nav>
+        
+        <SignInButton />
+      </div>
+    </header>
+  );
+}
