12eb01ea7fd5d6c70f71ecabac75581aa336489a
Walisson Silva <walissonsilva10@gmail.com>
2021-03-24 09:25:52 -0300

    chore: add integration with stripe api

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ig.news
 
-Link da Aplicação no Figma: [Acessar aqui.](https://www.figma.com/file/gl0fHkQgvaUfXNjuwGtDDs/ig.news?node-id=1%3A2)
+Link da Aplicação no Figma: [Acessar aqui.](https://www.figma.com/file/YM4JFm0d4b4TovqmCNrgRW/ig.news?node-id=1%3A2)
 
 ## Homepage da Aplicação no Figma
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
+
 import { SubscribeButton } from '../components/SubscribeButton';
+
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: Number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -16,14 +27,34 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get to access to all publications <br/>
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl Coding"/>
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IYUziEz76BSqA1pjpLFuLeX', {
+    expand: ['product'],
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product
+    },
+  }
 }
